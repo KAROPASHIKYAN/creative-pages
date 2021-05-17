b9b4bee5e53f2df79c2fd884cf1be95a7b93c7ac
KAROPASHIKYAN <notfound625@gmail.com>
2021-05-17 20:26:46 +0300

    html to wp

--- includes/sections/contact.php
+++ includes/sections/contact.php
@@ -1,21 +1,21 @@
-<section class="page-section" id="contact">
+<section class="page-section" id="<?php the_sub_field('section_name'); ?>">
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-lg-8 text-center">
-                <h2 class="mt-0">Let's Get In Touch!</h2>
+                <h2 class="mt-0"><?php the_sub_field('title'); ?></h2>
                 <hr class="divider my-4" />
-                <p class="text-muted mb-5">Ready to start your next project with us? Give us a call or send us an email and we will get back to you as soon as possible!</p>
+                <p class="text-muted mb-5"><?php the_sub_field('content'); ?></p>
             </div>
         </div>
         <div class="row">
             <div class="col-lg-4 ml-auto text-center mb-5 mb-lg-0">
                 <i class="fas fa-phone fa-3x mb-3 text-muted"></i>
-                <div>+1 (555) 123-4567</div>
+                <div><?php the_sub_field('phone_number'); ?></div>
             </div>
             <div class="col-lg-4 mr-auto text-center">
                 <i class="fas fa-envelope fa-3x mb-3 text-muted"></i>
                 <!-- Make sure to change the email address in BOTH the anchor text and the link target below!-->
-                <a class="d-block" href="mailto:contact@yourwebsite.com">contact@yourwebsite.com</a>
+                <a class="d-block" href="mailto:<?php the_sub_field('email'); ?>"><?php the_sub_field('email'); ?></a>
             </div>
         </div>
     </div>
