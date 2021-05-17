3026730b5980b830cc43df10f77827dbe36e5047
KAROPASHIKYAN <notfound625@gmail.com>
2021-05-17 20:12:58 +0300

    html to wp

--- includes/sections/portfolio.php
+++ includes/sections/portfolio.php
@@ -1,60 +1,20 @@
 <div id="portfolio">
     <div class="container-fluid p-0">
         <div class="row no-gutters">
-            <div class="col-lg-4 col-sm-6">
-                <a class="portfolio-box" href="<?= get_stylesheet_directory_uri(); ?>/assets/img/portfolio/fullsize/1.jpg">
-                    <img class="img-fluid" src="<?= get_stylesheet_directory_uri(); ?>/assets/img/portfolio/thumbnails/1.jpg" alt="..." />
-                    <div class="portfolio-box-caption">
-                        <div class="project-category text-white-50">Category</div>
-                        <div class="project-name">Project Name</div>
+            <?php $rows = get_sub_field('rows'); ?>
+            <?php if (!empty($rows)): ?>
+                <?php foreach ($rows as $key => $row): ?>
+                    <div class="col-lg-4 col-sm-6">
+                        <a class="portfolio-box" href="<?= $row['img_fullsize']['url'] ;?>">
+                            <img class="img-fluid" src="<?= $row['img_thumbnails']['url'] ;?>" alt="..." />
+                            <div class="portfolio-box-caption <?= $key == 5 ? 'p-3': '' ;?>">
+                                <div class="project-category text-white-50"><?= $row['category']; ?></div>
+                                <div class="project-name"><?= $row['project_name']; ?></div>
+                            </div>
+                        </a>
                     </div>
-                </a>
-            </div>
-            <div class="col-lg-4 col-sm-6">
-                <a class="portfolio-box" href="<?= get_stylesheet_directory_uri(); ?>/assets/img/portfolio/fullsize/2.jpg">
-                    <img class="img-fluid" src="<?= get_stylesheet_directory_uri(); ?>/assets/img/portfolio/thumbnails/2.jpg" alt="..." />
-                    <div class="portfolio-box-caption">
-                        <div class="project-category text-white-50">Category</div>
-                        <div class="project-name">Project Name</div>
-                    </div>
-                </a>
-            </div>
-            <div class="col-lg-4 col-sm-6">
-                <a class="portfolio-box" href="<?= get_stylesheet_directory_uri(); ?>/assets/img/portfolio/fullsize/3.jpg">
-                    <img class="img-fluid" src="<?= get_stylesheet_directory_uri(); ?>/assets/img/portfolio/thumbnails/3.jpg" alt="..." />
-                    <div class="portfolio-box-caption">
-                        <div class="project-category text-white-50">Category</div>
-                        <div class="project-name">Project Name</div>
-                    </div>
-                </a>
-            </div>
-            <div class="col-lg-4 col-sm-6">
-                <a class="portfolio-box" href="<?= get_stylesheet_directory_uri(); ?>/assets/img/portfolio/fullsize/4.jpg">
-                    <img class="img-fluid" src="<?= get_stylesheet_directory_uri(); ?>/assets/img/portfolio/thumbnails/4.jpg" alt="..." />
-                    <div class="portfolio-box-caption">
-                        <div class="project-category text-white-50">Category</div>
-                        <div class="project-name">Project Name</div>
-                    </div>
-                </a>
-            </div>
-            <div class="col-lg-4 col-sm-6">
-                <a class="portfolio-box" href="<?= get_stylesheet_directory_uri(); ?>/assets/img/portfolio/fullsize/5.jpg">
-                    <img class="img-fluid" src="<?= get_stylesheet_directory_uri(); ?>/assets/img/portfolio/thumbnails/5.jpg" alt="..." />
-                    <div class="portfolio-box-caption">
-                        <div class="project-category text-white-50">Category</div>
-                        <div class="project-name">Project Name</div>
-                    </div>
-                </a>
-            </div>
-            <div class="col-lg-4 col-sm-6">
-                <a class="portfolio-box" href="<?= get_stylesheet_directory_uri(); ?>/assets/img/portfolio/fullsize/6.jpg">
-                    <img class="img-fluid" src="<?= get_stylesheet_directory_uri(); ?>/assets/img/portfolio/thumbnails/6.jpg" alt="..." />
-                    <div class="portfolio-box-caption p-3">
-                        <div class="project-category text-white-50">Category</div>
-                        <div class="project-name">Project Name</div>
-                    </div>
-                </a>
-            </div>
+                <?php endforeach; ?>
+            <?php endif; ?>
         </div>
     </div>
 </div>
