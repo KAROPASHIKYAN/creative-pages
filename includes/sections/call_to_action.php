770a5b1443f368928057f99b708fe2beb78ecc8c
KAROPASHIKYAN <notfound625@gmail.com>
2021-05-17 20:56:49 +0300

    html to wp

--- includes/sections/call_to_action.php
+++ includes/sections/call_to_action.php
@@ -1,6 +1,9 @@
 <section class="page-section bg-dark text-white">
     <div class="container text-center">
-        <h2 class="mb-4">Free Download at Start Bootstrap!</h2>
-        <a class="btn btn-light btn-xl" href="https://startbootstrap.com/theme/creative/">Download Now!</a>
+        <h2 class="mb-4"><?php the_sub_field('title'); ?></h2>
+        <?php $link = get_sub_field('link'); ?>
+        <?php if (!empty($link)): ;?>
+            <a class="btn btn-light btn-xl" href="<?= $link['url']; ?>"><?= $link['title']; ?></a>
+        <?php endif; ?>
     </div>
 </section>
