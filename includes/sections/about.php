ac585494b0238fa5ab989222a60049849d521652
KAROPASHIKYAN <notfound625@gmail.com>
2021-05-18 01:42:23 +0300

    code refactoring

--- includes/sections/about.php
+++ includes/sections/about.php
@@ -1,11 +1,15 @@
-<section class="page-section bg-primary" id="<?php the_sub_field('section_name');?>">
+<?php
+$id = get_sub_field ('section_name');
+$button = get_sub_field ('button');
+?>
+<section class="page-section bg-primary" id="<?= register($id);?>">
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-lg-8 text-center">
                 <h2 class="text-white mt-0"><?php the_sub_field('title');?></h2>
                 <hr class="divider light my-4" />
                 <p class="text-white-50 mb-4"><?php the_sub_field('content');?></p>
-                <a class="btn btn-light btn-xl js-scroll-trigger" href="#<?php the_sub_field('button_link');?>"><?php the_sub_field('button_name');?></a>
+                <a class="btn btn-light btn-xl js-scroll-trigger" href="<?= register($button['url']) ;?>"><?= $button['title'];?></a>
             </div>
         </div>
     </div>
