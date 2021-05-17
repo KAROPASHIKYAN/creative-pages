e920d0c3d49776891b3138eaf3f25c593a4a0216
KAROPASHIKYAN <notfound625@gmail.com>
2021-05-17 16:01:46 +0300

    code refactoring

--- functions.php
+++ functions.php
@@ -13,16 +13,18 @@ add_action( 'wp_enqueue_scripts', function () {
     wp_enqueue_style('main', get_stylesheet_directory_uri() . '/css/styles.css', array(), time());
 
     wp_enqueue_script('jquery-3.5.1', 'https://code.jquery.com/jquery-3.5.1.min.js', array(), null, true);
+
     wp_enqueue_script('bundle', 'https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js', array(), null, true);
+
     wp_enqueue_script('anime', 'https://cdnjs.cloudflare.com/ajax/libs/animejs/3.2.1/anime.min.js', array(), null, true);
+
     wp_enqueue_script('magnific-popup', 'https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/jquery.magnific-popup.min.js', array(), null, true);
+    
     wp_enqueue_script('main', get_stylesheet_directory_uri() . '/js/scripts.js', array(), null, true);
 
 });
 add_action('after_setup_theme', function () {
 
     add_theme_support('title-tag');
-
-
 
 });
