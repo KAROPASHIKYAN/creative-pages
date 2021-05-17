46e19b5815bc1cf2c7c8e2e6384224ec99dbdf1c
KAROPASHIKYAN <notfound625@gmail.com>
2021-05-18 01:43:57 +0300

    code refactoring

--- index.php
+++ index.php
@@ -1,27 +1,23 @@
-<?php get_header();?>
+<?php get_header(); ?>
 <?php if (have_rows('sections')):
 
-while (have_rows('sections')): the_row();
+    while (have_rows('sections')): the_row();
 
-if (get_row_layout() == 'masthead') {
-sections('masthead');
-} elseif (get_row_layout() == 'about') {
-sections('about');
-} elseif (get_row_layout() == 'contact') {
-sections('contact');
-} elseif (get_row_layout() == 'portfolio') {
-sections('portfolio');
-} elseif (get_row_layout() == 'services') {
-sections('services');
-} elseif (get_row_layout() == 'call_to_action') {
-sections('call_to_action');
-}
+        if (get_row_layout() == 'masthead') {
+            sections('masthead');
+        } elseif (get_row_layout() == 'about') {
+            sections('about');
+        } elseif (get_row_layout() == 'contact') {
+            sections('contact');
+        } elseif (get_row_layout() == 'portfolio') {
+            sections('portfolio');
+        } elseif (get_row_layout() == 'services') {
+            sections('services');
+        } elseif (get_row_layout() == 'call_to_action') {
+            sections('call_to_action');
+        }
 
-endwhile;
+    endwhile;
 endif;
+get_footer();
 ?>
-<?php
-$sections = get_field('sections');
-
-?>
-<?php get_footer() ;?>
